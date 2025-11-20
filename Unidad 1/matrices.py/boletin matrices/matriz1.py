77bb23c1e0862849ce9c5db29650d0ba727ea15a
unknown <you@example.com>
2025-11-20 17:16:28 +0100

    Subo lo realizado en casa

--- Unidad 1/matrices.py/boletin matrices/matriz1.py
+++ Unidad 1/matrices.py/boletin matrices/matriz1.py
@@ -1,9 +1,9 @@
 matriz = [[8,1,6],[3,5,7],[4,9,2]]
-numfila = input("Dime el numero de fila: ")
-numcolumna = input("Dime el numero de la possición de elementos: ")
+numfila = int(input("Dime el numero de fila: "))
+numcolumna = int(input("Dime el numero de la columna: "))
 
 def dameelemento(matriz):
-    posicion= matriz[numfila[numcolumna]]
+    posicion= matriz[numfila][numcolumna]
     return posicion
 
 def damefila(matriz):
@@ -11,6 +11,14 @@ def damefila(matriz):
     return fila
 
 def damecolumna(matriz):
-    columna = matriz[numfila[numcolumna]]
-    if columna == "0":
-        
+    columna = [matriz[0][numcolumna], matriz[1][numcolumna], matriz[2][numcolumna]]
+    return columna
+
+elemento = dameelemento(matriz)
+print(elemento)
+
+filacompleta = damefila(matriz)
+print(filacompleta)
+
+columnacompleta = damecolumna(matriz)
+print(columnacompleta)
