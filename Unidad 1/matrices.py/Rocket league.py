07e1bc1d278671ad81c60739341d1789ee3e8d16
sgalrio0807 <sgalrio0807@g.educaand.es>
2025-12-04 09:17:23 +0100

    Subo lo realizado en clase

--- Unidad 1/matrices.py/Rocket league.py
+++ Unidad 1/matrices.py/Rocket league.py
@@ -9,64 +9,79 @@ def pintaMenu():
         opcion = input("Selecciona otra opción del menú: ").lower()
     return opcion
 
-def registroPuntuaciones(opcion):
-    eleccion = opcion
+def dameFase():
+    fase = input("Que fase quieres registrar(inicial, semifinal o final): ")
+    while fase not in ("inicial", "semifinal", "final"):
+        fase = input("Incorrecto, di de nuevo la fase que quieres registrar(inicial, semifinal o final): ")
+    return fase
+
+def registroPuntuaciones():
     listaequipos = []
     listapuntos = []
-    if eleccion == "r":
-        fase = input("Que fase quieres registrar(inicial, semifinal o final): ")
-        while fase not in ("inicial", "semifinal", "final"):
-            fase = input("Incorrecto, di de nuevo la fase que quieres registrar(inicial, semifinal o final): ")
-        if fase == "inicial":
-            for i in range(8):
-                equipos = input("Dime el nombre del equipo: ")
-                listaequipos.append(equipos)
-                puntos = int(input("Y su puntuación: "))
-                listapuntos.append(puntos)
-        elif fase == "semifinal":
-            for i in range(4):
-                equipos = input("Dime el nombre del equipo: ")
-                listaequipos.append(equipos)
-                puntos = int(input("Y su puntuación: "))
-                listapuntos.append(puntos)
-        else:
-            for i in range(2):
-                equipos = input("Dime el nombre del equipo: ")
-                listaequipos.append(equipos)
-                puntos = int(input("Y su puntuación: "))
-                listapuntos.append(puntos)
+    fase = dameFase()
+    if fase == "inicial":
+        for i in range(8):
+            equipos = input("Dime el nombre del equipo: ")
+            listaequipos.append(equipos)
+            puntos = int(input("Y su puntuación: "))
+            listapuntos.append(puntos)
+
+    elif fase == "semifinal":
+        for i in range(4):
+            equipos = input("Dime el nombre del equipo: ")
+            listaequipos.append(equipos)
+            puntos = int(input("Y su puntuación: "))
+            listapuntos.append(puntos)
+    else:
+        for i in range(2):
+            equipos = input("Dime el nombre del equipo: ")
+            listaequipos.append(equipos)
+            puntos = int(input("Y su puntuación: "))
+            listapuntos.append(puntos)
         print("===========================================")
         print("Datos registrados correctamente")
         print("===========================================")
     return fase, listaequipos, listapuntos
 
-def listarEquipos(opcion, fase_guardada, listaequipos, listapuntos):
-    eleccion = opcion
-    if eleccion == "l":
-        fase = input("Que fase quieres listar(inicial, semifinal o final): ")
-        while fase not in ("inicial", "semifinal", "final"):
-            print("===========================================")
-            print("La Fase", fase, "no ha sido registrada en el sistema")
-            print("===========================================")
-            fase = input("Di de nuevo la fase que quieres listar(inicial, semifinal o final): ")
-        
-        if fase != fase_guardada:
-            print("===========================================")
-            print("La Fase", fase, "no ha sido registrada en el sistema")
-            print("===========================================")
-        else:
-            print("===========================================")
-            print("Fase", fase)
-            print("===========================================")
-            for i in range(len(listaequipos)):
-                print("El equipo", listaequipos[i], "ha obtenido", listapuntos[i], "puntos")
-    return listaequipos, listapuntos
+puntuacionesfinal = []
+def listarEquipos(puntuacioninicio, puntuacionsemifinal, puntuacionesfinal, nombresinicio, nombressemifinal, nombresfinal ):
+        fase = dameFase()
+        if fase == "inicial":
+            nombresdeequipos = nombresinicio
+            puntuacionesequipos = puntuacioninicio
+        elif fase == "semifinal":
+            nombresdeequipos = nombressemifinal
+            puntuacionesequipos = puntuacionsemifinal
+        else: 
+            nombresdeequipos = nombresfinal
+            puntuacionesequipos = puntuacionesfinal
+
+        for i in range(0, len(nombresdeequipos)):
+            print ("El equipo ", nombresdeequipos[i]," ha obtenido ", puntuacionesequipos[i]," puntos")
+
+puntuacioninicio = []
+puntuacionsemifinal = []
+puntuacionesfinal = []
+nombresinicio = []
+nombressemifinal = []
+nombresfinal = []
 
 opcion = pintaMenu()
 print(opcion)
 
-registro = registroPuntuaciones(opcion)
+registro = registroPuntuaciones()
 print(registro)
 
-listar = listarEquipos(opcion, registro[0], registro[1], registro[2])
+fase = registro[0]
+if fase == "inicial":
+    nombresinicio = registro[1]
+    puntuacioninicio = registro[2]
+elif fase == "semifinal":
+    nombressemifinal = registro[1]
+    puntuacionsemifinal = registro[2]
+else:
+    nombresfinal = registro[1]
+    puntuacionesfinal = registro[2]
+
+listar = listarEquipos(puntuacioninicio, puntuacionsemifinal, puntuacionesfinal, nombresinicio, nombressemifinal, nombresfinal )
 print(listar)
